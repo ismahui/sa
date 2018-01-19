1fe189b39e1ead351162fa435f0b9477872e84d1
ismahui <ismahui@163.com>
2018-01-19 23:09:09 +0800

    引入okhttp

---
+++ app/src/main/java/com/mahui/sa/util/OkHttpManager.java
@@ -0,0 +1,58 @@
+package com.mahui.sa.util;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+/**
+ * Created by mahui on 2018/1/19.
+ */
+
+public class OkHttpManager {
+    private Object mLock = new Object();
+    private OkHttpClient mOkHttpClient;
+    private OkHttpManager mOkHttpManager;
+    private static final MediaType JSON
+            = MediaType.parse("application/json; charset=utf-8");
+
+    private OkHttpManager(){
+        mOkHttpClient = new OkHttpClient();
+        mOkHttpClient.newBuilder().connectTimeout(10, TimeUnit.SECONDS).readTimeout(10, TimeUnit.SECONDS)
+                .writeTimeout(10, TimeUnit.SECONDS);
+    }
+
+    public OkHttpManager getInstance() {
+        synchronized (mLock) {
+            if (mOkHttpManager == null) {
+                mOkHttpManager = new OkHttpManager();
+            }
+        }
+        return mOkHttpManager;
+    }
+
+    public  String get(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Response response = mOkHttpClient.newCall(request).execute();
+        return response.body().string();
+    }
+
+    public  String post(String url, String json) throws IOException {
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        Response response = mOkHttpClient.newCall(request).execute();
+        return response.body().string();
+    }
+
+
+}
